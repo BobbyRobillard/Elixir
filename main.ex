2c3b144e22d5c74ec9cecca38cf35e3b2b65781b
Bobby Robillard <robillard123@gmail.com>
2019-07-09 15:16:21 -0700

    Added Fibonnaci, and more to git ignore

--- main.ex
+++ main.ex
@@ -1,20 +1,36 @@
-defmodule GuessingGame do
-  def play(lower, upper) do
-    play(lower, upper, Enum.random(lower .. upper))
-  end
-  defp play(lower, upper, number) do
-    guess = Integer.parse(IO.gets "Guess a number (#{lower}-#{upper}): ")
-    case guess do
-      {^number, _} ->
-        IO.puts "Well guessed!"
-      {n, _} when n in lower..upper ->
-        IO.puts if n > number, do: "Too high.", else: "Too low."
-        play(lower, upper, number)
-      _ ->
-        IO.puts "Guess not in valid range."
-        play(lower, upper, number)
+###############################################################################
+#                               GUESSING GAME                                 #
+###############################################################################
+# defmodule GuessingGame do
+#   def play(lower, upper) do
+#     play(lower, upper, Enum.random(lower .. upper))
+#   end
+#   defp play(lower, upper, number) do
+#     guess = Integer.parse(IO.gets "Guess a number (#{lower}-#{upper}): ")
+#     case guess do
+#       {^number, _} ->
+#         IO.puts "Well guessed!"
+#       {n, _} when n in lower..upper ->
+#         IO.puts if n > number, do: "Too high.", else: "Too low."
+#         play(lower, upper, number)
+#       _ ->
+#         IO.puts "Guess not in valid range."
+#         play(lower, upper, number)
+#     end
+#   end
+# end
+#
+# GuessingGame.play(1, 100)
+###############################################################################
+#                                 FIBONACCI                                   #
+###############################################################################
+defmodule Fib do
+    def fib(n) do
+      case n do
+         {x} when x in 0..1 -> n
+          _ -> fib(n - 1) + fib(n - 2)
+      end
     end
-  end
 end
-
-GuessingGame.play(1, 100)
+IO.puts(Fib.fib(4))
+###############################################################################
