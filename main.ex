513e8f725e7c71bb1e633b993226fd3ca0fd6daf
Bobby Robillard <robillard123@gmail.com>
2019-07-18 15:46:17 -0700

    Added gcd, factorial

--- main.ex
+++ main.ex
@@ -1,26 +1,26 @@
 ###############################################################################
 #                               GUESSING GAME                                 #
 ###############################################################################
-# defmodule GuessingGame do
-#   def play(lower, upper) do
-#     play(lower, upper, Enum.random(lower .. upper))
-#   end
-#   defp play(lower, upper, number) do
-#     guess = Integer.parse(IO.gets "Guess a number (#{lower}-#{upper}): ")
-#     case guess do
-#       {^number, _} ->
-#         IO.puts "Well guessed!"
-#       {n, _} when n in lower..upper ->
-#         IO.puts if n > number, do: "Too high.", else: "Too low."
-#         play(lower, upper, number)
-#       _ ->
-#         IO.puts "Guess not in valid range."
-#         play(lower, upper, number)
-#     end
-#   end
-# end
-#
-# GuessingGame.play(1, 100)
+defmodule GuessingGame do
+  def play(lower, upper) do
+    play(lower, upper, Enum.random(lower .. upper))
+  end
+  defp play(lower, upper, number) do
+    guess = Integer.parse(IO.gets "Guess a number (#{lower}-#{upper}): ")
+    case guess do
+      {^number, _} ->
+        IO.puts "Well guessed!"
+      {n, _} when n in lower..upper ->
+        IO.puts if n > number, do: "Too high.", else: "Too low."
+        play(lower, upper, number)
+      _ ->
+        IO.puts "Guess not in valid range."
+        play(lower, upper, number)
+    end
+  end
+end
+
+GuessingGame.play(1, 100)
 ###############################################################################
 #                                 FIBONACCI                                   #
 ###############################################################################
