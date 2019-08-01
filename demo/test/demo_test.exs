99bf17c9a280cdc5aa27b0b18b9ea5ada2743e91
Bobby Robillard <robillard123@gmail.com>
2019-07-31 19:50:43 -0700

    Command line parsing library beginnings

--- demo/test/demo_test.exs
+++ demo/test/demo_test.exs
@@ -2,7 +2,15 @@ defmodule DemoTest do
   use ExUnit.Case
   doctest Demo
 
-  test "greets the world" do
-    assert Demo.hello() == :world
+  import Demo.CLI, only: [ parse_args: 1 ]
+
+  test ":help returned by option parsing with -h and --help options" do
+    assert parse_args(["-h", "anything"]) == :help
+    assert parse_args(["--help", "anything"]) == :help
   end
+
+  test "three values returned if three given" do
+    assert parse_args(["user", "project", "99"]) == { "user", "project", 99 }
+  end
+
 end
