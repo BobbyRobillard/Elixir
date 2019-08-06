b2d085b4749ee5961e92421d7a0e7eb6c41395e0
Bobby Robillard <robillard123@gmail.com>
2019-08-06 10:21:48 -0700

    Update largest-prime.ex

--- largest-prime.ex
+++ largest-prime.ex
@@ -1,47 +1,46 @@
 defmodule LargestPrime do
-
+  # ----------------------------------------------------------
   defp find_factors(num), do: _find_factors(num, 2, [])
 
-  defp _find_factors(num, at, factors) when at > num/2, do: factors
-
   defp _find_factors(num, at, factors) do
-    if rem(num, at) == 0 do # Is a factor
-      _find_factors(num, at + 1, factors ++ [at])
+    # Functions with potential exceptions can't be used as guard clauses
+    # ------------------------------------------------------------------
+    # Cool little trick to reduce execution time
+    # https://www.youtube.com/watch?v=oFahMlzI-Ic
+    if at > :math.sqrt(num) do
+      factors
     else
-      _find_factors(num, at + 1, factors)
+      if rem(num, at) != 0 do # Not a factor
+        _find_factors(num, at + 1, factors)
+      else
+        if num/at == at do
+          _find_factors(num, at + 1, factors ++ [at])
+        else
+          _find_factors(num, at + 1, factors ++ [at, num/at])
+        end
+      end
     end
   end
+  # ----------------------------------------------------------
+  defp get_largest_prime([]), do: "No Primes"
 
-  defp is_prime(num) do
-    case find_factors(num) do
-      [] -> True
-      [_head|_tail]  -> False
+  defp get_largest_prime([head|tail]) do
+    case find_factors(Kernel.trunc(head)) do # Check if head is prime
+      [] -> head
+      _ -> get_largest_prime(tail)
     end
   end
+  # ----------------------------------------------------------
 
-  defp find_largest_prime([]), do: "No Primes"
-
-  defp find_largest_prime([head|tail]) do
-    case is_prime(head) do
-      True -> head
-      False -> find_largest_prime(tail)
-    end
-  end
-
-  def find(num) do
+  def find(num) do # Find largest prime
     find_factors(num)
     |> Enum.sort(&(&1 > &2))
-    |> find_largest_prime
+    |> get_largest_prime
     |> IO.puts
   end
 end
 
-
-# 1) Determine the factors of a number
-# 2) Determine all the prime factors up to the largest factor
-# 3) Sort the list of prime factors and return the largest
+# Main execution of the program
 IO.gets("Number to find largest prime of: ")
-|> String.trim
-|> String.to_integer
-|> LargestPrime.find
+|> String.trim |> String.to_integer |> LargestPrime.find
 |> IO.puts
