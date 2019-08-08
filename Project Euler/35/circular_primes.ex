04e6ecba3f7dcba3d6de4e49e4f5b41aa0de5012
Bobby Robillard <robillard123@gmail.com>
2019-08-08 00:36:43 -0700

    Cleaned up problem 35

--- Project Euler/35/circular_primes.ex
+++ Project Euler/35/circular_primes.ex
@@ -2,31 +2,22 @@ defmodule CircularPrime do
   def find_amount do
     x =
     2..100
-    |> Enum.map_reduce([], fn n, acc -> (
-                              if is_prime(n) do
-                                {n, acc ++ [n]}
-                              else {n, acc} end
-                           ) end
-                       ) # Get all primes below a number
+    |> Enum.map_reduce([], fn n, acc -> is_prime(n, acc) end)
     |> elem(1)
-    |> Enum.map_reduce([], fn n, acc -> (
-                              if is_circular(n) do
-                                {n, acc ++ [n]}
-                              else {n, acc} end
-                          ) end
-    )
+    |> Enum.map_reduce([], fn n, acc -> is_circular(n, acc) end)
     |> elem(1)
     IO.inspect binding()
     # |> Enum.sum
   end
 
+  defp is_prime(x, acc) do
     case find_factors x do
-      [] -> :true
-      _  -> :false
+      [] -> {x, acc ++ [x]}
+       _ -> {x, acc}
     end
   end
 
-  defp is_circular(x) do :true end
+  defp is_circular(x, acc) do {x, acc ++ [x]} end
 
   defp find_factors(num), do: _find_factors(num, 2, [])
 
