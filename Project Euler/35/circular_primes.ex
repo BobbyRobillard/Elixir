33887429911c376074967ff4bc259ba6c55ed4fd
Bobby Robillard <robillard123@gmail.com>
2019-08-08 00:37:17 -0700

    Update circular_primes.ex

--- Project Euler/35/circular_primes.ex
+++ Project Euler/35/circular_primes.ex
@@ -2,22 +2,22 @@ defmodule CircularPrime do
   def find_amount do
     x =
     2..100
-    |> Enum.map_reduce([], fn n, acc -> is_prime(n, acc) end)
+    |> Enum.map_reduce([], fn n, acc -> add_if_prime(n, acc) end)
     |> elem(1)
-    |> Enum.map_reduce([], fn n, acc -> is_circular(n, acc) end)
+    |> Enum.map_reduce([], fn n, acc -> add_if_circular(n, acc) end)
     |> elem(1)
     IO.inspect binding()
     # |> Enum.sum
   end
 
-  defp is_prime(x, acc) do
+  defp add_if_prime(x, acc) do
     case find_factors x do
       [] -> {x, acc ++ [x]}
        _ -> {x, acc}
     end
   end
 
-  defp is_circular(x, acc) do {x, acc ++ [x]} end
+  defp add_if_circular(x, acc) do {x, acc ++ [x]} end
 
   defp find_factors(num), do: _find_factors(num, 2, [])
 
