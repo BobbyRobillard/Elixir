bb71edb81b411393961307cc03048922ba5ec65c
Bobby Robillard <robillard123@gmail.com>
2019-08-08 23:54:26 -0700

    Update circular_primes.ex

--- Project Euler/35/circular_primes.ex
+++ Project Euler/35/circular_primes.ex
@@ -1,13 +1,16 @@
 defmodule CircularPrime do
+
+  # 1) Find all prime numbers in 1 -> n
+  # 2) From all primes find if each is circular
+  # 3) Count number of circular primes
+
   def find_amount do
-    x =
-    2..1_000_000
+    2..1000
     |> Enum.map_reduce([], fn n, acc -> add_if_prime(n, acc) end)
     |> elem(1)
     |> Enum.map_reduce([], fn n, acc -> add_if_circular(n, acc) end)
     |> elem(1)
-    IO.inspect binding()
-    # |> Enum.sum
+    |> Enum.count
   end
 
   defp add_if_prime(x, acc) do
@@ -17,7 +20,15 @@ defmodule CircularPrime do
      end
   end
 
-  defp add_if_circular(x, acc) do {x, acc ++ [x]} end
+  defp add_if_circular(x, acc) do
+    x
+    |> Integer.to_string
+    |> String.split("")
+    |> List.delete_at(0)
+    |> List.delete_at(-1)
+    
+    # {x, acc ++ [x]}
+  end
 
   defp has_factors(x) do
      if rem(x, 2) == 0 or rem(x, 3) == 0
@@ -53,4 +64,4 @@ end
 
 # Main execution of the program
 CircularPrime.find_amount
-# |> IO.puts
+|> IO.puts
