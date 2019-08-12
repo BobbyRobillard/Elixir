f56d002ed49045a3e122697040ce5e910e9b0eda
Bobby Robillard <robillard123@gmail.com>
2019-08-12 12:29:33 -0700

    Create pandigital_primes.ex

---
+++ Project Euler/32/pandigital_primes.ex
@@ -0,0 +1,41 @@
+defmodule PandigitalPrimes do
+  def sum do
+    1..2000
+    |> Enum.reduce([], fn x, acc -> acc ++ pandigital(x) end)
+    |> Enum.uniq
+    |> Enum.sum
+  end
+
+  def pandigital(input) do
+    1..2000
+    |> Enum.reduce([], fn x, acc -> (if check_sum(input, x) do acc ++ [x * input] else acc end) end)
+  end
+
+  def check_sum(input, x) do
+    digits = [input * x, input, x]
+    |> Enum.join
+    |> String.split("", trim: true)
+    |> Enum.sort
+
+
+    if Enum.count(digits) == 9 do
+      valid_pandigital = 1..9
+      |> Enum.join
+      |> String.split("", trim: true)
+
+      if valid_pandigital == digits do
+        :true
+      else
+        :false
+      end
+
+    else
+      :false
+    end
+
+  end
+
+end
+
+PandigitalPrimes.sum
+|> IO.puts
