af7a26c8d284b5418d5093dfab1d7800ff97e349
Robert Robillard <BobbyRobillard@users.noreply.github.com>
2019-08-12 01:00:27 -0400

    Update c-constant.ex

--- Project Euler/40/c-constant.ex
+++ Project Euler/40/c-constant.ex
@@ -1,11 +1,8 @@
 defmodule CConstant do
   def find do
-    lst = 1..300_000
-    |> Enum.map(fn x -> Integer.to_string x end)
-    |> List.to_string
-    |> String.split("")
-    |> List.delete_at(0)
-    |> List.delete_at(-1)
+    lst = 1..200_000
+    |> Enum.join
+    |> String.split("", trim: true)
 
     [0, 9, 99, 999, 9_999, 99_999, 999_999]
     |> Enum.reduce(1, fn x, res -> res * String.to_integer(Enum.at lst, x) end)
